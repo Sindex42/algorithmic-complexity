81590092c59cccc2eedcd8321dcab011db27f99d
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-16 17:40:12 +0100

    Add basic timing framework

---
+++ src/TimingFunctions.js
@@ -0,0 +1,13 @@
+const benchmark = (func, input) => {
+  let start = process.hrtime()
+  func(input)
+  let timeElapsed = process.hrtime(start)
+
+  return timeElapsed
+}
+
+const randomArray = (length, max) => {
+  return Array(length).fill().map(() => Math.round(Math.random() * max))
+}
+
+module.exports = { benchmark, randomArray }
