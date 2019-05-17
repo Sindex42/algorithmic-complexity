834fbb8525d9ad0cc0f3a8f1a60446061ccbe25b
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-17 12:41:50 +0100

    framework writes results of benchmark to .csv in increments of 5000

--- framework.js
+++ framework.js
@@ -3,6 +3,7 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter
 const { lastElement } = require('./src/Functions.js')
 const { benchmark, randomArray } = require('./src/TimingFunctions')
 
+// CSV file template
 const csvWriter = createCsvWriter({
   path: 'results.csv',
   header: [
@@ -11,13 +12,19 @@ const csvWriter = createCsvWriter({
   ]
 })
 
-let array = randomArray(10000)
-let time = benchmark(lastElement, array)
+// Benchmark execution
+let records = []
 
-const records = [
-  {size: `${array.length}`,  time: `${time[1]}`}
-]
+for (var i = 5000; i <= 100000; i += 5000) {
+  let array = randomArray(i)
+  let time = benchmark(lastElement, array)
 
+  records.push(
+    {size: i,  time: (time[0], time[1])}
+  )
+}
+
+// Write to file
 csvWriter
   .writeRecords(records)
   .then(() => { console.log('Data written to "results.csv"') })
