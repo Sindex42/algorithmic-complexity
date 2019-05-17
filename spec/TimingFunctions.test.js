7ff130b933a6888b083e89331f2b317afe625acf
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-17 16:10:29 +0100

    Add tests for #randomArray

--- spec/TimingFunctions.test.js
+++ spec/TimingFunctions.test.js
@@ -1,4 +1,4 @@
-const { findMedian } = require('../src/TimingFunctions.js')
+const { findMedian, randomArray } = require('../src/TimingFunctions.js')
 
 describe('#findMedian', () => {
   test('finds the median value of an array (odd)', () => {
@@ -9,5 +9,15 @@ describe('#findMedian', () => {
   test('finds the median value of an array (even)', () => {
     let array = [0, 50, 2, 10, 12, 30]
     expect(findMedian(array)).toEqual(11)
+  })
+})
+
+describe('#randomArray', () => {
+  test('creates an array of the specified length', () => {
+    expect(randomArray(5).length).toEqual(5)
+  })
+
+  test('creates an array where the max value is the length', () => {
+    expect(randomArray(5).sort()[4]).toBeLessThanOrEqual(5)
   })
 })
