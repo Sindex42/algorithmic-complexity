b44cb65b43964e01011218db3dfa7c357e7a1c06
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-21 16:58:50 +0100

    Add test for fisher yates shuffle

--- spec/Functions.test.js
+++ spec/Functions.test.js
@@ -1,8 +1,18 @@
-const { lastElement } = require('../src/Functions.js')
+const f = require('../src/Functions.js')
 
 describe('#last', () => {
   test('returns the last element of an array', () => {
-    let array = [ 8, 5, 6, 5, 3 ]
-    expect(lastElement(array)).toEqual(3)
+    let array = [8, 5, 6, 5, 3]
+    expect(f.lastElement(array)).toEqual(3)
+  })
+})
+
+describe('#shuffleFY', () => {
+  test('returns a shuffled array', () => {
+    let original = [1, 2, 3, 4, 5]
+    let array = [1, 2, 3, 4, 5]
+
+    expect(f.shuffleFY(array)).not.toEqual(original)
+    expect(f.shuffleFY(array)).toEqual(expect.arrayContaining(original))
   })
 })
