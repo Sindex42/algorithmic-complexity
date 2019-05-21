c46f5f35e90b3efddd21e8220f3806aba0c124dd
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-21 16:39:55 +0100

    Add .shuffle and data

--- framework.js
+++ framework.js
@@ -5,7 +5,7 @@ const timeF = require('./src/TimingFunctions')
 const [START, MAX, STEP] = [5000, 100000, 5000]
 const WARMUPS = 100 // default: 100
 const WARMUP_ITERATIONS = 10 // default: 10
-const TEST_FUNCTION = testF.lastElement
+const TEST_FUNCTION = testF.shuffleFY
 const TEST_ITERATIONS = 100 // default: 100
 
 // CSV file template
@@ -26,9 +26,13 @@ const benchLoop = (recordBool, iterations) => {
     if (recordBool) { timeF.printIterationNr('Array size', i) }
 
     for (let j = 0; j < iterations ;j++) {
-      timings.push(timeF.benchmark(TEST_FUNCTION, timeF.randomArray(i)))
+      timings.push(
+        timeF.benchmark(TEST_FUNCTION, timeF.randomArray(i))
+      )
     }
-    if (recordBool) { records.push({ size: i,  time: timeF.findMedian(timings) }) }
+    if (recordBool) {
+      records.push({ size: i,  time: timeF.findMedian(timings) })
+    }
   }
   return records
 }
@@ -38,10 +42,10 @@ for (let i = 0; i < WARMUPS; i++) {
   benchLoop(false, WARMUP_ITERATIONS)
   timeF.printIterationNr('Warmups', i + 1)
 }
+
 console.log('\nWarmups complete')
 
 // Benchmark and write to file
 csvWriter
   .writeRecords(benchLoop(true, TEST_ITERATIONS))
   .then(() => { console.log('\nData written to "output.csv"') })
-  .catch(() => { console.log('\nUnable to write data to file') })
--- src/Functions.js
+++ src/Functions.js
@@ -1,5 +1,20 @@
+// shuffle, reverse, find duplicates
+
 const lastElement = (array) => array[array.length - 1]
 
+const shuffleFY = (array) => {
+  var j, x, i
+  for (i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1))
+    x = array[i]
+    array[i] = array[j]
+    array[j] = x
+  }
+  return array
+}
+
+
 module.exports = {
-  lastElement
+  lastElement,
+  shuffleFY
 }
