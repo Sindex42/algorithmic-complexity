b72f4677d0553d51459f2c93d818b34f627a4681
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-21 18:21:24 +0100

    Bug: #mergeSort sorts, sometimes...

--- spec/Functions.test.js
+++ spec/Functions.test.js
@@ -18,12 +18,21 @@ describe('#shuffleFY', () => {
 })
 
 describe('#mergeSort', () => {
-  test('returns an array of length 1', () => {
+  test('sorts an array of length 1', () => {
     expect(f.mergeSort([1])).toEqual([1])
+  })
+
+  test('sorts an array of length 2', () => {
+    expect(f.mergeSort([2, 1])).toEqual([1, 2])
   })
 
   test('returns a sorted array', () => {
     let array = [8, 5, 6, 5, 3]
     expect(f.mergeSort(array)).toEqual([3, 5, 5, 6, 8])
+  })
+
+  test.only('returns a sorted array', () => {
+    let array = [ 7, 2, 8, 3, 9, 8, 9, 2, 4, 3 ]
+    expect(f.mergeSort(array)).toEqual([2, 2, 3, 3, 4, 7, 8, 8, 9, 9])
   })
 })
--- src/Functions.js
+++ src/Functions.js
@@ -14,10 +14,51 @@ const shuffleFY = (arr) => {
 }
 
 const mergeSort = (arr) => {
+  let left, right, sortedLeft, sortedRight, iL, iR, result
   l = arr.length
 
   if (l < 2) {
     return arr
+  } else {
+    // breakdown
+    left = arr.slice(0, l / 2)
+    right = arr.slice(l / 2)
+    console.log('l, r', left, right)
+
+    // recursive call
+    sortedLeft = mergeSort(left)
+    sortedRight = mergeSort(right)
+
+    // merging
+    iL = 0
+    iR = 0
+    result = []
+
+    // console.log(iL, iR, sortedLeft.length, sortedRight.length)
+
+    while (iL < sortedLeft.length && iR < sortedRight.length) {
+      if (sortedLeft[iL] <= sortedRight[iR]) {
+        console.log('left smaller, result before', result)
+        result.push(sortedLeft[iL])
+        iL++
+        console.log('index, result after', iL, result)
+      } else {
+        console.log('right smaller, result before', result)
+        result.push(sortedRight[iR])
+        iR++
+        console.log('index, result after:', iR, result)
+      }
+    }
+
+    if (iR = sortedRight.length) {
+      result.push(sortedLeft.slice(iL))
+      console.log('after push right', result)
+    } else if (iL = sortedLeft.length) {
+      result.push(sortedRight.slice(iR))
+      console.log('after push left', result)
+    }
+
+    return result.flat()
   }
 }
 
